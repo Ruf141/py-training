566569a2581cbedccb53ecbe4cbda35b553cfbb5
Kazuki.Ishii <kazuki.ishii@milab-inc.com>
2024-10-05 12:16:32 +0900

    commit

--- Chapter2/2-1/average_1.py
+++ Chapter2/2-1/average_1.py
@@ -1,5 +1,5 @@
-score = [70, 98 ,92, 88, 64]
-total =0
+score = [70, 98, 92, 88, 64]
+total = 0
 for i in range(5):
     total = total + score[i]
 average = total / 5
--- Chapter2/2-1/average_2.py
+++ Chapter2/2-1/average_2.py
@@ -2,6 +2,6 @@ score = [70, 98, 92, 88, 64]
 total = 0
 for i in score:
     total = total + i
-    avarage = total / 5
+    average = total / 5
 print("合計点", total)
-print("平均点", avarage)
+print("平均点", average)
