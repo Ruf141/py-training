0e198c9a1e0d0a5d3bd7cb0568cae552e2dfdf57
Ruf141 <mad0908141@gmail.com>
2024-10-08 18:11:10 +0900

    commit

--- LeetCode/roman_to_int.py
+++ LeetCode/roman_to_int.py
@@ -1,15 +1,28 @@
 def roman_to_int(s: str) -> int:
+    # ローマ数字とその値を対応付ける辞書を定義
+    roman_map = {
+        "I": 1, "V": 5, "X": 10, 
+        "L": 50, "C": 100, "D": 500, 
+        "M": 1000
+    }
 
-    roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
+    # 合計値を保持する変数
     total = 0
 
+    # ローマ数字を順に処理するループ
     for i in range(len(s)):
+        # 現在のローマ数字の値を取得
         current = roman_map[s[i]]
+        
+        # 次の文字が存在し、かつ現在の値より大きい場合、減算が必要
+        # 例: "IV"や"IX"のようなケース
         if i + 1 < len(s) and current < roman_map[s[i + 1]]:
+            # 減算して合計に反映
             total -= current
         else:
+            # 通常の場合、加算して合計に反映
             total += current
+            
     return total
 
 # テストケース
